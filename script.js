c44230c530ad13ffabd160d92be3f7d5735dad6b
Friedrich Hemer <friedrich.hemer@gmail.com>
2020-04-12 07:25:26 +0000

    css checkable hover consistency

--- script.js
+++ script.js
@@ -31,18 +31,42 @@ function initHouses() {
   }
 }
 
+function initBonus() {
+  for (var i in bonus) {
+    $("#widget-container").append(`<div id='bonus-${i}' class='bonus widget' style='left:${xscale*bonus[i].x}%;top:${yscale*bonus[i].y}%;'><input type="number" min="0" max="9" value="0"></div>`);
+  }
+};
+
 function initParks() {
+  var extraClass = "";
+  var row = 99;
   for (var i in parks) {
-    $("#widget-container").append(`<div id='park-${i}' class='park widget' style='left:${xscale*parks[i].x}%;top:${yscale*parks[i].y}%;' data-row='${parks[i].row}' data-score='${parks[i].score}'></div>`);
+    if(row != parks[i].row) {
+      row = parks[i].row;
+      extraClass = "checkable";
+    }
+    $("#widget-container").append(`<div id='park-${i}' class='park widget ${extraClass}' style='left:${xscale*parks[i].x}%;top:${yscale*parks[i].y}%;' data-row='${parks[i].row}' data-score='${parks[i].score}'></div>`);
+    extraClass = "";
   }
   for (var i in park_scores) {
     $("#widget-container").append(`<div id='park-score-${i}' class='total-score park-score widget' style='left:${xscale*park_scores[i].x}%;top:${yscale*park_scores[i].y}%;'><span></span></div>`);
   }
 }
 
+function updateParkScores() {
+  for (var row in park_scores) {
+    var last_checked = $(".park[data-row='" + row +"'].check:last");
+    if($(last_checked).length==0) {
+      $("#park-score-"+row+">span").text(0);
+    } else {
+      $("#park-score-"+row+">span").text($(last_checked).data('score'));
+    }
+  }
+}
+
 function initPools() {
   for (var i in pools) {
-    $("#widget-container").append(`<div id='pool-${i}' class='pool widget' style='left:${xscale*pools[i].x}%;top:${yscale*pools[i].y}%;'></div>`);
+    $("#widget-container").append(`<div id='pool-${i}' class='pool widget checkable' style='left:${xscale*pools[i].x}%;top:${yscale*pools[i].y}%;'></div>`);
   }
   for (var i in pools_checks) {
     $("#widget-container").append(`<div id='pool-check-${i}' class='pool-check widget' style='left:${xscale*pools_checks[i].x}%;top:${yscale*pools_checks[i].y}%;'></div>`);
@@ -60,13 +84,20 @@ function initWorkSigns() {
     $("#widget-container").append(`<div id='work-${i}' class='work widget' style='left:${xscale*worksigns[i].x}%;top:${yscale*worksigns[i].y}%;'></div>`);
   }
   for (var i in worksigns_radio) {
-    $("#widget-container").append(`<div id='work-score-${i}' class='work-score widget' data-score='${worksigns_radio[i].score}' style='left:${xscale*worksigns_radio[i].x}%;top:${yscale*worksigns_radio[i].y}%;'><span></span></div>`);
+    $("#widget-container").append(`<div id='work-score-${i}' class='work-score widget checkable' data-score='${worksigns_radio[i].score}' style='left:${xscale*worksigns_radio[i].x}%;top:${yscale*worksigns_radio[i].y}%;'><span></span></div>`);
   }
 }
 
 function initLandPrices() {
+  var extraClass = "";
+  var col = 99;
   for (var i in landprices) {
-    $("#widget-container").append(`<div id='landprice-${i}' class='landprice widget' data-col='${landprices[i].col}' data-score='${landprices[i].score}' style='left:${xscale*landprices[i].x}%;top:${yscale*landprices[i].y}%;'></div>`);
+    if(col != landprices[i].col) {
+      col = landprices[i].col;
+      extraClass = "checkable";
+    }
+    $("#widget-container").append(`<div id='landprice-${i}' class='landprice widget ${extraClass}' data-col='${landprices[i].col}' data-score='${landprices[i].score}' style='left:${xscale*landprices[i].x}%;top:${yscale*landprices[i].y}%;'></div>`);
+    extraClass = "";
   }
   for (var i in landcounters) {
     $("#widget-container").append(`<div id='landcounter-${i}' class='landcounter widget' style='left:${xscale*landcounters[i].x}%;top:${yscale*landcounters[i].y}%;'><input type="number" min="0" max="9" value="0"></div>`);
@@ -85,8 +116,24 @@ function updateLandScores() {
 }
 
 function initBis() {
+  var extraClass = "";
   for (var i in bis) {
-    $("#widget-container").append(`<div id='bis-${i}' class='bis widget' style='left:${100*bis[i].x}%;top:${100*bis[i].y}%;'></div>`);
+    if (i==0) {
+      extraClass = "checkable";
+    }
+    $("#widget-container").append(`<div id='bis-${i}' class='bis widget ${extraClass}' style='left:${100*bis[i].x}%;top:${100*bis[i].y}%;'></div>`);
+    extraClass = "";
+  }
+}
+
+function initCantplay() {
+  var extraClass = "";
+  for (var i in cantplay) {
+    if (i==0) {
+      extraClass = "checkable";
+    }
+    var res = $("#widget-container").append(`<div id='cantplay-${i}' class='cantplay widget ${extraClass}' data-score='${cantplay[i].score}' style='left:${xscale*cantplay[i].x}%;top:${yscale*cantplay[i].y}%;'></div>`);
+    extraClass = "";
   }
 }
 
@@ -94,6 +141,23 @@ function initScores() {
   for (var i in total_scores) {
     $("#widget-container").append(`<div id='${total_scores[i].id}' class='total-score widget' style='left:${xscale*total_scores[i].x}%;top:${yscale*total_scores[i].y}%;'><span></span></div>`);
   }
+}
+
+function updateTotalScore() {
+  var sum = 0;
+  sum+= Number($("#bonus-score>span").text());
+  sum+= Number($("#park-score>span").text());
+  sum+= Number($("#pool-score>span").text());
+  sum+= Number($("#work-score>span").text());
+  sum+= Number($("#landprice-score-0>span").text());
+  sum+= Number($("#landprice-score-1>span").text());
+  sum+= Number($("#landprice-score-2>span").text());
+  sum+= Number($("#landprice-score-3>span").text());
+  sum+= Number($("#landprice-score-4>span").text());
+  sum+= Number($("#landprice-score-5>span").text());
+  sum-= Number($("#bis-score>span").text());
+  sum-= Number($("#cantplay-score>span").text());
+  $('#total-score>span').text(sum);
 }
 
 function shuffle(array, seed) {
@@ -178,12 +242,14 @@ function generateDeck() {
 
 $( document ).ready(function() {
     initHouses();
+    initBonus();
     initParks();
     initPools();
     initFences();
     initWorkSigns();
     initLandPrices();
     initBis();
+    initCantplay();
     initScores();
 
     $("#seed").val(Math.floor((Math.random() * $("#seed").attr("max")) + 1));
@@ -195,30 +261,38 @@ $( document ).ready(function() {
       console.log($(this).attr('id'));
     });
 
+    $(".bonus>input").on('input',function(){
+      var sum = 0;
+      $('.bonus>input').each(function() {
+          sum += Number($(this).val());
+      });
+      $('#bonus-score>span').text(sum);
+      updateTotalScore();
+    });
+
     $(".park").click(function() {
       var row = $(this).data("row");
       var first_unchecked = $(".park[data-row='" + row +"']:not(.check):first");
       var last_checked = $(".park[data-row='" + row +"'].check:last");
         
       if (($(this).hasClass('check') && $(last_checked).data('score') === $(this).data('score')) || (!$(this).hasClass('check') && $(first_unchecked).data('score') == $(this).data('score'))) {
-        var result = $(this).toggleClass("check");
-        if (result.hasClass("check")) {
-          $("#park-score-"+row+">span").text($(this).data('score'));
-        } else {
-          if ($(this).data('score')==2) {
-            $("#park-score-"+row+">span").text(0);
-          } else {
-            $("#park-score-"+row+">span").text($(".park[data-row='" + row +"'].check:last").data('score'));
-          }
-        }
+        $(this).toggleClass("check");
+
+        $(".park[data-row='" + row +"']").each(function(){$(this).removeClass("checkable");});
+        $(".park[data-row='" + row +"']:not(.check):first").addClass("checkable");
+        $(".park[data-row='" + row +"'].check:last").addClass("checkable");
+
+        updateParkScores();
       } else {
         return false;
       }
+
       var sum = 0;
       $('.park-score>span').each(function() {
           sum += Number($(this).text());
       });
       $('#park-score>span').text(sum);
+      updateTotalScore();
     });
 
     $(".pool").click(function() {
@@ -229,6 +303,7 @@ $( document ).ready(function() {
         $(".pool-check.check:last").toggleClass("check");
       }
       $("#pool-score>span").text(pool_scores[$(".pool-check.check").length]);
+      updateTotalScore();
     });
 
     $(".fence").click(function() {
@@ -251,6 +326,7 @@ $( document ).ready(function() {
       } else {
         $("#work-score>span").text(0);
       }
+      updateTotalScore();
     });
 
     $(".landprice").click(function() {
@@ -262,18 +338,21 @@ $( document ).ready(function() {
         
       if (($(this).hasClass('check') && $(last_checked).data('score') === $(this).data('score')) || (!$(this).hasClass('check') && $(first_unchecked).data('score') == $(this).data('score'))) {
         $(this).toggleClass("check");
+
+        $(".landprice[data-col='" + col +"']").each(function(){$(this).removeClass("checkable");});
+        $(".landprice[data-col='" + col +"']:not(.check):first").addClass("checkable");
+        $(".landprice[data-col='" + col +"'].check:last").addClass("checkable");
+
         updateLandScores();
       } else {
         return false;
       }
+      updateTotalScore();
     });
     
     $(".landcounter>input").change(function(){
       updateLandScores();
-    });
-
-    $(".total-score").change(function() {
-      console.log("chanaaaage");
+      updateTotalScore();
     });
 
     $(".bis").click(function() {
@@ -284,13 +363,42 @@ $( document ).ready(function() {
         
       if (($(this).hasClass('check') && $(last_checked).attr('id') === $(this).attr('id')) || (!$(this).hasClass('check') && $(first_unchecked).attr('id') == $(this).attr('id'))) {
         $(this).toggleClass("check");
+        $(".bis").each(function(){$(this).removeClass("checkable");});
+        $(".bis:not(.check):first").addClass("checkable");
+        $(".bis.check:last").addClass("checkable");
         $("#bis-score>span").text(bis_scores[$(".bis.check").length]);
+        updateTotalScore();
+      } else {
+        return false;
+      }
+    });
+
+    $(".cantplay").click(function() {
+      var first_unchecked = $(".cantplay:not(.check):first");
+      var last_checked = $(".cantplay.check:last");
+        
+      if (($(this).hasClass('check') && $(last_checked).attr('id') === $(this).attr('id')) || (!$(this).hasClass('check') && $(first_unchecked).attr('id') == $(this).attr('id'))) {
+        $(this).toggleClass("check");
+        var new_last_checked = $(".cantplay.check:last");
+        if($(new_last_checked).length==0) {
+          $("#cantplay-score>span").text(0);  
+        } else {
+          $("#cantplay-score>span").text($(new_last_checked).data('score'));
+        }
+        $(".cantplay").each(function(){$(this).removeClass("checkable");});
+        $(".cantplay:not(.check):first").addClass("checkable");
+        $(".cantplay.check:last").addClass("checkable");
+        updateTotalScore();
       } else {
         return false;
       }
     });
 
     $(".total-score").each(function() {
+      $("span", this).text(0);
+    });
+
+    $(".bonus-score").each(function() {
       $("span", this).text(0);
     });
 
