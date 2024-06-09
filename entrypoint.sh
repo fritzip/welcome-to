690aa045525a8298e3179805760ec5a23cf720c0
Maxime Sainlot <maxime.sainlot@gmail.com>
2024-06-09 23:20:55 +0200

    fix: replace $VERSION in index.html in place

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Replace placeholders with environment variables in place in /usr/share/nginx/html/index.html
-envsubst '${VERSION}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+envsubst '${VERSION}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
 
 # Start nginx
 nginx -g 'daemon off;'
