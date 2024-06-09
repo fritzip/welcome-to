c91d34419220dfe6474386b8de0534919945dcd7
Maxime Sainlot <maxime.sainlot@gmail.com>
2024-06-09 23:34:57 +0200

    feat: $RELEASE_URL substituted in index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # Replace placeholders with environment variables in place in /usr/share/nginx/html/index.html
-envsubst '${VERSION}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+envsubst '${VERSION} ${RELEASE_URL}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
 mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
-
 
 # Start nginx
 nginx -g 'daemon off;'
