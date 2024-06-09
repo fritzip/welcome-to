90f1eb62b0f44b7ee21b12ad871d5c7f3ad4ba31
Maxime Sainlot <maxime.sainlot@gmail.com>
2024-06-09 22:53:46 +0200

    chore: remove .template extension to work locally
    ci: retrieve version name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Replace placeholders with environment variables
-envsubst '${VERSION}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+# Replace placeholders with environment variables in place in /usr/share/nginx/html/index.html
+envsubst '${VERSION}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
 
 # Start nginx
 nginx -g 'daemon off;'
